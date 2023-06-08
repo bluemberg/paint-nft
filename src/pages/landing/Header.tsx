a167032e0f9ae8ee3371228f1aa008f935fe370c
Blooming Dan Moneda <bamoneda@up.edu.ph>
2023-06-08 17:34:12 +0800

    Change themes

--- src/pages/landing/Header.tsx
+++ src/pages/landing/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const THEMES = ["light", "dark", "cupcake"];
+const THEMES = ["light", "dark"];
 
 export default function Header() {
   const [theme, setTheme] = React.useState('light');
