e0fbfcefc01952f6736c4f7f58ef4d95e748ab22
Blooming Dan Moneda <bamoneda@up.edu.ph>
2023-06-08 17:48:12 +0800

    Add features pane to landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -3,12 +3,14 @@ import React from "react";
 import Header from "./landing/Header";
 import Hero from "./landing/Hero";
 import Footer from "../components/Footer";
+import Info from "./landing/Info";
 
 const LandingPage = () => {
   return (
     <nav>
       <Header></Header>
       <Hero></Hero>
+      <Info></Info>
       <Footer></Footer>
     </nav>
   );
---
+++ src/pages/landing/Info.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+export default function Services() {
+  return (
+    <section className='py-10 md:py-16'>
+      <div className='container'>
+        <div className='text-center'>
+          <h2 className='text-3xl sm:text-5xl font-bold mb-4'>Features</h2>
+          <p className='text-lg sm:text-2xl mb-6 md:mb-14'>
+            PaintNFT is a web platform that allows users to paint their own NFTs and trade in the marketplace.
+          </p>
+        </div>
+
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 lg:gap-8 xl:gap-10'>
+          <div className="card bg-base-200 transform-gpu transition-all duration-300 hover:-translate-y-2 hover:shadow-lg">
+            <div className="card-body items-center text-center gap-4">
+              <i className='bi bi-search text-4xl'></i>
+              <h2 className="card-title">Canvas</h2>
+              <p>
+                This is a wider card with <br className='hidden xl:inline' />supporting text below as a <br className='hidden xl:inline' /> natural content.
+              </p>
+            </div>
+          </div>
+
+          <div className="card bg-base-200 transform-gpu transition-all duration-300 hover:-translate-y-2 hover:shadow-lg">
+            <div className="card-body items-center text-center gap-4">
+              <i className='bi bi-chat-left-dots text-4xl'></i>
+              <h2 className="card-title">Marketplace</h2>
+              <p>
+                This is a wider card with <br className='hidden xl:inline' />supporting text below as a <br className='hidden xl:inline' /> natural content.
+              </p>
+            </div>
+          </div>
+
+          <div className="card bg-base-200 transform-gpu transition-all duration-300 hover:-translate-y-2 hover:shadow-lg">
+            <div className="card-body items-center text-center gap-4">
+              <i className='bi bi-badge-ad text-4xl'></i>
+              <h2 className="card-title">Bla bla bla</h2>
+              <p>
+                This is a wider card with <br className='hidden xl:inline' />supporting text below as a <br className='hidden xl:inline' /> natural content.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
