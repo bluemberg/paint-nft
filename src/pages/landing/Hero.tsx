57c269ef8fcb4f03c081563b95211d6dd876903c
Blooming Dan Moneda <bamoneda@up.edu.ph>
2023-06-08 22:02:42 +0800

    Add themes

--- src/pages/landing/Hero.tsx
+++ src/pages/landing/Hero.tsx
@@ -4,17 +4,17 @@ export default function Hero() {
   return (
     <section>
       <div className='container'>
-        <div className="hero h-96 md:h-[500px] rounded-box overflow-hidden" style={{ backgroundImage: `url("https://img.freepik.com/free-vector/gradient-isometric-nft-concept_52683-62009.jpg?w=996&t=st=1686213831~exp=1686214431~hmac=0ece240e71ab1dc8179065b9c913db781fb4e32c18133b74c9bfe2f19d7520cd")` }}>
-          <div className="hero-overlay bg-opacity-80"></div>
-          <div className="hero-content text-center text-primary-content">
+        <div className="hero h-96 md:h-[500px] rounded-box overflow-hidden" style={{ backgroundImage: `url("https://img.freepik.com/free-vector/vintage-vaporwave-background_23-2148886069.jpg?w=996&t=st=1686232901~exp=1686233501~hmac=41e59c90884fe56d3e66d601608a4af0d86465ace371208637a634df15c556d5")` }}>
+          <div className="hero-overlay bg-opacity-60 bg-secondary"></div>
+          <div className="hero-content text-center text-secondary-content">
             <div className="max-w-lg">
-              <h1 className="mb-5 sm:mb-7 text-8xl sm:text-8xl font-bold">
+              <h1 className="mb-5 sm:mb-7 text-4xl sm:text-8xl font-bold">
                 PaintNFT
               </h1>
-              <p className="mb-5 sm:mb-7 sm:text-xl">
+              <p className="mb-5 sm:mb-7 sm:text-2xl">
                 Your art, your way.
               </p>
-              <button className="btn btn-info sm:btn-wide">Connect Wallet</button>
+              <button className="btn btn-warning sm:btn-wide">Connect Wallet</button>
             </div>
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,7 @@ module.exports = {
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    themes: ["light", "dark", "cupcake", "pastel"],
+    styled: true,
+    themes: ["dark", "light", "cupcake", "pastel", "cyberpunk"],
   },
 };
