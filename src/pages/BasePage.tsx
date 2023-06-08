5c68724ebf844cca321139d42d1c724039c0f65a
Blooming Dan Moneda <bamoneda@up.edu.ph>
2023-06-08 16:14:22 +0800

    Update BasePage

--- src/pages/BasePage.tsx
+++ src/pages/BasePage.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 interface Props {
   children: React.ReactNode;
 }
 const BasePage = (props: Props) => {
   return (
-    <Navbar></Navbar>
+    <nav>
+      <Navbar></Navbar>
+      <Footer></Footer>
+    </nav>
   );
 };
 
