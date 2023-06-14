7d998cb1a041c8edb07153ea119f0d0a3f8899a7
Blooming Dan Moneda <bamoneda@up.edu.ph>
2023-06-14 17:41:00 +0800

    `Hero.tsx`: Change to local image

--- src/pages/landing/Hero.tsx
+++ src/pages/landing/Hero.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useContext } from "react";
-
+import img from "../../img/hero_bg.jpg";
 import AccountContext from "../../context/account-context";
 
 export default function Hero() {
@@ -11,7 +11,7 @@ export default function Hero() {
         <div
           className="hero h-96 md:h-[500px] rounded-box overflow-hidden"
           style={{
-            backgroundImage: `url("https://img.freepik.com/free-vector/vintage-vaporwave-background_23-2148886069.jpg?w=996&t=st=1686232901~exp=1686233501~hmac=41e59c90884fe56d3e66d601608a4af0d86465ace371208637a634df15c556d5")`,
+            backgroundImage: `url('${img}')`,
           }}
         >
           <div className="hero-overlay bg-opacity-60 bg-secondary"></div>
