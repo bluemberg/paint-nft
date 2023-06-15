274411fd36ebd2830e473d0571cc73c1ad12dc51
JulianDy <jufiejuf@gmail.com>
2023-06-15 07:13:50 -0400

    convert marketplace elements to daisyui components

--- src/pages/MarketplacePage.tsx
+++ src/pages/MarketplacePage.tsx
@@ -24,6 +24,10 @@ const MarketplacePage = () => {
 
   const openModal = (nft: NFT) => {
     setSelectedNft(nft);
+    const modal = document.getElementById('my_modal_1') as HTMLDialogElement | null;
+    if (modal) {
+      modal.showModal();
+    }
   };
 
   const closeModal = () => {
@@ -51,31 +55,37 @@ const MarketplacePage = () => {
 
       {/* GRID OF NFTS */}
       <div className="flex justify-center items-center h-full px-4">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 mt-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 mt-10">
           {nfts.map((nft: NFT, index) => (
             <div
               key={index}
-              className="bg-white border border-gray-200 rounded-lg p-4 cursor-pointer hover:shadow-lg"
+              className="card card-bordered card-normal w-150 bg-base-100 shadow-xl"
               onClick={() => openModal(nft)}
             >
-              <img
+              <figure>
+                <img
                 src={nft.token_info.artifactUri}
                 alt={nft.token_info.name}
-                className="mb-4 rounded-lg"
-              />
-              <h2 className="text-lg font-semibold mb-2">
-                {nft.token_info.name}
-              </h2>
-              <p className="text-gray-600 mb-1">
-                Author: {nft.token_info.minter}
-              </p>
-              <p className="text-gray-600 mb-1">Price: {nft.amount}</p>
+                />
+              </figure>
+
+              <div className="card-body">
+                <h2 className="card-title">
+                  {nft.token_info.name}
+                </h2>
+
+                <p className="truncate"><b>Author:</b>  {nft.token_info.minter}</p>
+                <p><b>Description:</b> {nft.token_info.description}</p>
+                <p><b>Creators:</b>  {nft.token_info.creators.join(', ')}</p>
+                <p><b>Tags:</b> {nft.token_info.tags.join(', ')}</p>
+                <p><b>Price:</b>  {nft.amount}</p>
+              </div>
             </div>
           ))}
         </div>
       </div>
 
-      {/* MODAL/CARD FOR EACH NFT */}
+      {/* MODAL/CARD FOR EACH NFT
       {selectedNft && (
         <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75">
           <div className="bg-white p-4 rounded-lg max-w-sm overflow-y-auto max-h-full mt-20">
@@ -130,7 +140,58 @@ const MarketplacePage = () => {
             </div>
           </div>
         </div>
+      )} */}
+
+      {/* MODAL FOR EACH CARD */}
+      {selectedNft &&(
+        <dialog id="my_modal_1" className="modal">
+          <form method="dialog" className="modal-box">
+            <figure>
+                <img
+                src={selectedNft.token_info.artifactUri}
+                alt={selectedNft.token_info.name}
+                />
+              </figure>
+
+              <div className="card-body">
+                <h2 className="card-title">
+                  {selectedNft.token_info.name}
+                </h2>
+
+                <p className="truncate"><b>Author:</b>  {selectedNft.token_info.minter}</p>
+                <p><b>Description:</b> {selectedNft.token_info.description}</p>
+                <p><b>Creators:</b>  {selectedNft.token_info.creators.join(', ')}</p>
+                <p><b>Tags:</b> {selectedNft.token_info.tags.join(', ')}</p>
+                <p><b>Price:</b>  {selectedNft.amount}</p>
+              </div>
+            <div className="modal-action">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Close</button>
+              <button className={
+                "btn-primary"
+                + (selectedNft.token_info.minter === accountContext.address
+                  ? "btn-disabled"
+                  : "btn-primary")
+                 }
+                 onClick={() =>
+                  toast.promise(
+                    buyNft(
+                      parseInt(selectedNft.token_id),
+                      parseInt(selectedNft.amount) + 10_00000 // one 1 tez to the price for gas fee
+                    ),
+                    {
+                      loading: "Buying NFT.",
+                      success: "Successfully bought NFT!",
+                      error: "Error buying NFT.",
+                    }
+                  )
+                }>
+                Buy</button>
+            </div>
+          </form>
+        </dialog>
       )}
+
     </BasePage>
   );
 };
