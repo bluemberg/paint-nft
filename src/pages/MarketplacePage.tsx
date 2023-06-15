336ba4a11cf5613a3430d21d199312320bf7577a
JulianDy <jufiejuf@gmail.com>
2023-06-15 07:21:15 -0400

    marketplace daisui components

--- src/pages/MarketplacePage.tsx
+++ src/pages/MarketplacePage.tsx
@@ -74,7 +74,7 @@ const MarketplacePage = () => {
                   {nft.token_info.name}
                 </h2>
 
-                <p className="truncate"><b>Author:</b>  {nft.token_info.minter}</p>
+                <p><b>Author:</b>  {nft.token_info.minter}</p>
                 <p><b>Description:</b> {nft.token_info.description}</p>
                 <p><b>Creators:</b>  {nft.token_info.creators.join(', ')}</p>
                 <p><b>Tags:</b> {nft.token_info.tags.join(', ')}</p>
@@ -84,63 +84,6 @@ const MarketplacePage = () => {
           ))}
         </div>
       </div>
-
-      {/* MODAL/CARD FOR EACH NFT
-      {selectedNft && (
-        <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75">
-          <div className="bg-white p-4 rounded-lg max-w-sm overflow-y-auto max-h-full mt-20">
-            <button
-              className="absolute top-2 right-2 text-gray-600 hover:text-gray-900"
-              onClick={closeModal}
-            >
-              Close
-            </button>
-            <img
-              src={selectedNft.token_info.artifactUri}
-              alt={selectedNft.token_info.name}
-              className="mb-4 rounded-lg"
-            />
-            <h2 className="text-lg font-semibold mb-2">
-              {selectedNft.token_info.name}
-            </h2>
-            <p className="text-gray-600 mb-1">
-              Author: {selectedNft.token_info.minter}
-            </p>
-            <p className="text-gray-600 mb-1">Price: {selectedNft.amount}</p>
-            <div className="flex justify-end">
-              <button
-                className="btn btn-secondary py-2 px-4 rounded mr-2"
-                onClick={closeModal}
-              >
-                Close
-              </button>
-              <button
-                className={
-                  "btn  text-white py-2 px-4 rounded " +
-                  (selectedNft.token_info.minter === accountContext.address
-                    ? "btn-disabled"
-                    : "btn-primary")
-                }
-                onClick={() =>
-                  toast.promise(
-                    buyNft(
-                      parseInt(selectedNft.token_id),
-                      parseInt(selectedNft.amount) + 10_00000 // one 1 tez to the price for gas fee
-                    ),
-                    {
-                      loading: "Buying NFT.",
-                      success: "Successfully bought NFT!",
-                      error: "Error buying NFT.",
-                    }
-                  )
-                }
-              >
-                Buy
-              </button>
-            </div>
-          </div>
-        </div>
-      )} */}
 
       {/* MODAL FOR EACH CARD */}
       {selectedNft &&(
