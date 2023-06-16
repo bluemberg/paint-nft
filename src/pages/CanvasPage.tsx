560a6e8ce700400c8b1d109e00fe773f37ec384f
Blooming Dan Moneda <bamoneda@up.edu.ph>
2023-06-16 16:18:15 +0800

    style: remove text canvas page

--- src/pages/CanvasPage.tsx
+++ src/pages/CanvasPage.tsx
@@ -6,7 +6,13 @@ import Canvas from "../components/Canvas";
 const CanvasPage = () => {
   return (
     <BasePage>
-      <h1 className="text-center text-4xl">Canvas Page</h1>
+    
+      {/* <h1 className="text-left text-6xl font-bold pl-24 pt-12">Canvas</h1>
+
+      <div className="px-24">
+        <div className="divider"></div>
+      </div> */}
+
       <Canvas></Canvas>
     </BasePage>
   );
